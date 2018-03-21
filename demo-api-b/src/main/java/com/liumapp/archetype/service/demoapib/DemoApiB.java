848d576e32d13e69492a93b27ac9a325565766fa
liumapp <liumapp.com@gmail.com>
2018-03-21 17:14:12 +0800

    get demo-api-b done & add eureka & add config dependency

--- demo-api-b/src/main/java/com/liumapp/archetype/service/demoapib/DemoApiB.java
+++ demo-api-b/src/main/java/com/liumapp/archetype/service/demoapib/DemoApiB.java
@@ -1,4 +1,4 @@
-package com.liumapp.archetype.service.demoapia;
+package com.liumapp.archetype.service.demoapib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,17 +6,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liumapp
- * @file DemoApiA.java
+ * @file DemoApiB.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 3/21/18
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class DemoApiA {
+public class DemoApiB {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApiA.class , args);
+        SpringApplication.run(DemoApiB.class , args);
     }
 
 }
